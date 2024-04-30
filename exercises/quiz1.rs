0ba049e34f7275c6bf0ce7c62f0c6e83c4d35eb1
guessg <149642415+guessg@users.noreply.github.com>
2024-04-30 23:20:30 +0800

    part1

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,14 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(x: i32) -> i32 {
+    if x <= 40 {
+        2 * x
+    } else {
+        x
+    }
+}
 
 // Don't modify this function!
 #[test]
--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -20,8 +20,6 @@
 //
 // No hints this time!
 
-// I AM NOT DONE
-
 pub enum Command {
     Uppercase,
     Trim,
@@ -32,11 +30,23 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
         // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+        let mut output: Vec<String> = vec![];
         for (string, command) in input.iter() {
-            // TODO: Complete the function body. You can do it!
+            let mut new_string = string.clone();
+            match command {
+                Command::Uppercase => {
+                    new_string = new_string.to_uppercase();
+                }
+                Command::Trim => {
+                    new_string = new_string.trim().to_string();
+                }
+                Command::Append(n) => {
+                    new_string = new_string + &"bar".repeat(*n);
+                }
+            }
+            output.push(new_string);
         }
         output
     }
@@ -45,7 +55,7 @@ mod my_module {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
